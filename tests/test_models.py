b0a67bc5f7946ecea7b097d87c847949fc161d08
Srinivas SP <srijaipur@gmail.com>
2025-04-16 23:21:06 -0400

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,106 +1,249 @@
-# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2023 John Rofrano. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 # https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 """
-Test cases for Product Model
+Models for Product Demo Service
 
-Test cases can be run with:
-    nosetests
-    coverage report -m
+All of the models are stored in this module
 
-While debugging just these tests it's convenient to use this:
-    nosetests --stop tests/test_models.py:TestProductModel
+Models
+------
+Product - A Product used in the Product Store
+
+Attributes:
+-----------
+name (string) - the name of the product
+description (string) - the description the product belongs to (i.e., dog, cat)
+available (boolean) - True for products that are available for adoption
 
 """
-import os
 import logging
-import unittest
+from enum import Enum
 from decimal import Decimal
-from service.models import Product, Category, db
-from service import app
-from tests.factories import ProductFactory
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-DATABASE_URI = os.getenv(
-    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
-)
+logger = logging.getLogger("flask.app")
+
+# Create the SQLAlchemy object to be initialized later in init_db()
+db = SQLAlchemy()
 
 
-######################################################################
-#  P R O D U C T   M O D E L   T E S T   C A S E S
-######################################################################
-# pylint: disable=too-many-public-methods
-class TestProductModel(unittest.TestCase):
-    """Test Cases for Product Model"""
+def init_db(app):
+    """Initialize the SQLAlchemy app"""
+    Product.init_db(app)
 
-    @classmethod
-    def setUpClass(cls):
-        """This runs once before the entire test suite"""
-        app.config["TESTING"] = True
-        app.config["DEBUG"] = False
-        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-        app.logger.setLevel(logging.CRITICAL)
-        Product.init_db(app)
 
-    @classmethod
-    def tearDownClass(cls):
-        """This runs once after the entire test suite"""
-        db.session.close()
+class DataValidationError(Exception):
+    """Used for an data validation errors when deserializing"""
 
-    def setUp(self):
-        """This runs before each test"""
-        db.session.query(Product).delete()  # clean up the last tests
+
+class Category(Enum):
+    """Enumeration of valid Product Categories"""
+
+    UNKNOWN = 0
+    CLOTHS = 1
+    FOOD = 2
+    HOUSEWARES = 3
+    AUTOMOTIVE = 4
+    TOOLS = 5
+
+
+class Product(db.Model):
+    """
+    Class that represents a Product
+
+    This version uses a relational database for persistence which is hidden
+    from us by SQLAlchemy's object relational mappings (ORM)
+    """
+
+    ##################################################
+    # Table Schema
+    ##################################################
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    price = db.Column(db.Numeric, nullable=False)
+    available = db.Column(db.Boolean(), nullable=False, default=True)
+    category = db.Column(
+        db.Enum(Category), nullable=False, server_default=(Category.UNKNOWN.name)
+    )
+
+    ##################################################
+    # INSTANCE METHODS
+    ##################################################
+
+    def __repr__(self):
+        return f"<Product {self.name} id=[{self.id}]>"
+
+    def create(self):
+        """
+        Creates a Product to the database
+        """
+        logger.info("Creating %s", self.name)
+        # id must be none to generate next primary key
+        self.id = None  # pylint: disable=invalid-name
+        db.session.add(self)
         db.session.commit()
 
-    def tearDown(self):
-        """This runs after each test"""
-        db.session.remove()
+    def update(self):
+        """
+        Updates a Product to the database
+        """
+        logger.info("Saving %s", self.name)
+        if not self.id:
+            raise DataValidationError("Update called with empty ID field")
+        db.session.commit()
 
-    ######################################################################
-    #  T E S T   C A S E S
-    ######################################################################
+    def delete(self):
+        """Removes a Product from the data store"""
+        logger.info("Deleting %s", self.name)
+        db.session.delete(self)
+        db.session.commit()
 
-    def test_create_a_product(self):
-        """It should Create a product and assert that it exists"""
-        product = Product(name="Fedora", description="A red hat", price=12.50, available=True, category=Category.CLOTHS)
-        self.assertEqual(str(product), "<Product Fedora id=[None]>")
-        self.assertTrue(product is not None)
-        self.assertEqual(product.id, None)
-        self.assertEqual(product.name, "Fedora")
-        self.assertEqual(product.description, "A red hat")
-        self.assertEqual(product.available, True)
-        self.assertEqual(product.price, 12.50)
-        self.assertEqual(product.category, Category.CLOTHS)
+    def serialize(self) -> dict:
+        """Serializes a Product into a dictionary"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": str(self.price),
+            "available": self.available,
+            "category": self.category.name  # convert enum to string
+        }
 
-    def test_add_a_product(self):
-        """It should Create a product and add it to the database"""
-        products = Product.all()
-        self.assertEqual(products, [])
-        product = ProductFactory()
-        product.id = None
-        product.create()
-        # Assert that it was assigned an id and shows up in the database
-        self.assertIsNotNone(product.id)
-        products = Product.all()
-        self.assertEqual(len(products), 1)
-        # Check that it matches the original product
-        new_product = products[0]
-        self.assertEqual(new_product.name, product.name)
-        self.assertEqual(new_product.description, product.description)
-        self.assertEqual(Decimal(new_product.price), product.price)
-        self.assertEqual(new_product.available, product.available)
-        self.assertEqual(new_product.category, product.category)
+    def deserialize(self, data: dict):
+        """
+        Deserializes a Product from a dictionary
+        Args:
+            data (dict): A dictionary containing the Product data
+        """
+        try:
+            self.name = data["name"]
+            self.description = data["description"]
+            self.price = Decimal(data["price"])
+            if isinstance(data["available"], bool):
+                self.available = data["available"]
+            else:
+                raise DataValidationError(
+                    "Invalid type for boolean [available]: "
+                    + str(type(data["available"]))
+                )
+            self.category = getattr(Category, data["category"])  # create enum from string
+        except AttributeError as error:
+            raise DataValidationError("Invalid attribute: " + error.args[0]) from error
+        except KeyError as error:
+            raise DataValidationError("Invalid product: missing " + error.args[0]) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid product: body of request contained bad or no data " + str(error)
+            ) from error
+        return self
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    ##################################################
+    # CLASS METHODS
+    ##################################################
+
+    @classmethod
+    def init_db(cls, app: Flask):
+        """Initializes the database session
+
+        :param app: the Flask app
+        :type data: Flask
+
+        """
+        logger.info("Initializing database")
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
+
+    @classmethod
+    def all(cls) -> list:
+        """Returns all of the Products in the database"""
+        logger.info("Processing all Products")
+        return cls.query.all()
+
+    @classmethod
+    def find(cls, product_id: int):
+        """Finds a Product by it's ID
+
+        :param product_id: the id of the Product to find
+        :type product_id: int
+
+        :return: an instance with the product_id, or None if not found
+        :rtype: Product
+
+        """
+        logger.info("Processing lookup for id %s ...", product_id)
+        return cls.query.get(product_id)
+
+    @classmethod
+    def find_by_name(cls, name: str) -> list:
+        """Returns all Products with the given name
+
+        :param name: the name of the Products you want to match
+        :type name: str
+
+        :return: a collection of Products with that name
+        :rtype: list
+
+        """
+        logger.info("Processing name query for %s ...", name)
+        return cls.query.filter(cls.name == name)
+
+    @classmethod
+    def find_by_price(cls, price: Decimal) -> list:
+        """Returns all Products with the given price
+
+        :param price: the price to search for
+        :type name: float
+
+        :return: a collection of Products with that price
+        :rtype: list
+
+        """
+        logger.info("Processing price query for %s ...", price)
+        price_value = price
+        if isinstance(price, str):
+            price_value = Decimal(price.strip(' "'))
+        return cls.query.filter(cls.price == price_value)
+
+    @classmethod
+    def find_by_availability(cls, available: bool = True) -> list:
+        """Returns all Products by their availability
+
+        :param available: True for products that are available
+        :type available: str
+
+        :return: a collection of Products that are available
+        :rtype: list
+
+        """
+        logger.info("Processing available query for %s ...", available)
+        return cls.query.filter(cls.available == available)
+
+    @classmethod
+    def find_by_category(cls, category: Category = Category.UNKNOWN) -> list:
+        """Returns all Products by their Category
+
+        :param category: values are ['MALE', 'FEMALE', 'UNKNOWN']
+        :type available: enum
+
+        :return: a collection of Products that are available
+        :rtype: list
+
+        """
+        logger.info("Processing category query for %s ...", category.name)
+        return cls.query.filter(cls.category == category)
